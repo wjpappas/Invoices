09ea23e54490c652f68b4c0b77e3701b7ece1982
wjpappas <wjpappas@gmail.com>
2023-01-15 21:24:17 -0800

    " tuning"

--- rd_dev.py
+++ rd_dev.py
@@ -22,8 +22,6 @@ ifile_name = sys.argv[2]
 cust_file = sys.argv[3]
 
 logging.basicConfig(filename='invoices.log', level=logging.DEBUG, format='%(lineno)d - %(funcName)s - %(levelname)s - %(message)s')
-#logging.basicConfig(filename='invoices.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-#std_file = 'Invfile/prod_file.txt'
 outputname_x = r'(\S+).txt'
 outname = ((re.compile(outputname_x)).search(ifile_name)).group(1)+'.csv'
 outputFile = open(outname, 'w')
@@ -46,7 +44,6 @@ term_key = ['terms']
 cash_x = r'(INVOICE)'
 eat_color = r'^\s+ T PAINT.*'
 scc_x =  r'(\s*\d+\s+)(SUPPLY CHAIN CHRG)(\s+\d+\s+[-]*[\d]+[.][\d]{2}[\ *]*\s+)([-]*[\d]+[.][\d]{2})([N]*)'
-#overhead_x = r'22-000'
 
 def _get_overhead():
     """Fetch the overhead job code from your configuration file.
@@ -62,7 +59,7 @@ def _get_overhead():
     thisyear = config["overhead_code"]["OH_thisyear"]
     lastyear = config["overhead_code"]["OH_lastyear"]
     std_file = config["file_spec"]["product"]
-    return (thisyear, lastyear, std_file)
+    return [thisyear, lastyear, std_file]
 
 def read_vendor(file):
     """Read vendor file into working lists."""
@@ -138,24 +135,23 @@ def f_cust_job(m7):
 
 def read_std_file(val, sfile):
     """Read to list "Material' product file for Standard."""
-    if val == '9':
+    if val == '4':
         std_list = []
         with open(sfile, 'r') as std_read:
             while True:
                 line = std_read.readline()
                 if not line:
                     break
-                std_list.append(line)
+                std_list.append(line.rstrip())
         return std_list
 
 def std_item(memo, s_list):
     """Determine item value for Standard."""
-    value = True
+    value = False
     for p_str in s_list:
-        if ((memo.lower()).find(p_str) != -1):
-            value = False
-            break
-    logging.debug('product %s; match: %s', memo, value)
+        if (memo.lower()).find(p_str) != -1:
+            value = True
+    logging.debug('product %s; T/F: %s %s', memo, value, type(p_str))
     return value
 
 def item_ck(item_val, par_01, par_02):
@@ -199,23 +195,19 @@ def prt_value(item, qty, desc, value, r_dict, p_list):
         print_record(p_list[1], r_dict, p_list[2])
 
 oh_codes = _get_overhead()
-#overhead_last = str(oh_codes[1])
-#overhead_now = str(oh_codes[0])
 overhead_now, overhead_last, std_file = oh_codes
 overhead_x = r'%s'%overhead_now
 logging.debug('OH codes: %s overhead_x: %s', oh_codes, overhead_x)
 cust_dict = bh.make_dict(cust_file, overhead_now)
-print(cust_dict['22-000'])
 header_dict = set_dict(record_keys, qb_header)
 print_record(outputWriter, header_dict, record_keys)
 prt_list = [update_keys, outputWriter, record_keys]
 
 vendor_val, reg_val, grp_val = read_vendor(ifile_vendor)
-#vendor_data = read_vendor(ifile_vendor)
-#vendor_val, reg_val, grp_val = vendor_data
-#vendor_val = vendor_data[0]
-#reg_val = vendor_data[1]
-#grp_val = vendor_data[2]
+salenum_rgx, backend_rgx, color_rgx, disc_rgx, tax_rgx, pca_rgx = reg_val[8:]
+print(reg_val[8:13])
+pca_grp, item_grp, qty_grp, tax_grp, price_grp = grp_val
+print(grp_val)
 record_dict = set_dict(record_keys, qb_record)
 update_dict(record_dict, vendor_keys, vendor_val)
 i_terms = vendor_val[1]
@@ -233,31 +225,32 @@ with open(ifile_name, 'r') as reader:
     while True:
         straye.clear()
         db = None
-        while not db:
+        while not db:                                   # read  header block
             haye = reader.readline()
             db = desc_b.search(haye)
             straye.append(haye)
-        head_x = find_header_x(listx, straye, f_head)
+        head_x = find_header_x(listx, straye, f_head)   # invoice header
+        cust_job = head_x[4]
+        itSign = credit_ck(head_x[-1], term_key, [i_terms], ['Credit'])
         logging.debug('headx =' + str(head_x))
 
         update_dict(record_dict, headr_keys, head_x)
 
-        memo_len = 30
-        rec_pos = db.start()
-        cust_job = head_x[4]
+        memo_len = 40
+        rec_pos = db.start(1)
         item_val = re.compile(overhead_x).search(cust_job)
         item_supply = item_ck(item_val, 'Shop Supplies', 'Supplies')
         logging.debug('DB position: %s; DB: %s', rec_pos, db)
-        itSign = credit_ck(head_x[-1], term_key, [i_terms], ['Credit'])
         disc_total = 0
         pca_total = 0
         scc_total = 0
+        haye = reader.readline()
         haye_last = haye
 
         while True:
-            pca_temp = re.compile(reg_val[13]).search(haye)
+            pca_temp = re.compile(pca_rgx).search(haye)
             if pca_temp:
-                pca_total += float(pca_temp.group(int(grp_val[0])))
+                pca_total += float(pca_temp.group(int(pca_grp)))
                 haye = reader.readline()
 
             scc_temp = re.compile(scc_x).search(haye)
@@ -265,64 +258,68 @@ with open(ifile_name, 'r') as reader:
                 scc_total += float(scc_temp.group(4))
                 haye = reader.readline()
 
-            if re.compile(reg_val[8]).search(haye):             # sales number
+            if re.compile(salenum_rgx).search(haye):             # sales number
                 memo = (haye[rec_pos:rec_pos+memo_len]).strip()
-                back_end = re.compile(reg_val[9]).search(haye)  # backend
-                if grp_val[1] == '9':
+                back_end = re.compile(backend_rgx).search(haye)  # backend
+                logging.debug('item value: %s, nine? %s', item_val, item_grp)
+                if item_grp == '4':
                     item_val = std_item(memo, std_list)
                 else:
-                    item_val = back_end.group(int(grp_val[1]))
-                item = item_ck(item_val, 'Materials', item_supply)
-                logging.debug('item value: %s, item-supply: %s', type(item_val), item_supply)
-                quantity = back_end.group(int(grp_val[2]))
-                price = float((back_end.group(int(grp_val[4]))).replace(",", ""))*itSign
+                    if item_grp == '3':
+                        par01, par02 = item_supply, 'Materials'
+                    if item_grp == '7':
+                        par01, par02 = 'Materials', item_supply
+                    item_val = back_end.group(int(item_grp))
+                item = item_ck(item_val, par01, par02)
+                logging.debug('item value: %s, item-supply: %s', item_val, item_supply)
+                quantity = back_end.group(int(qty_grp))
+                price = float((back_end.group(int(price_grp))).replace(",", ""))*itSign
                 haye = reader.readline()
                 if re.compile(eat_color).match(haye):
                     haye = reader.readline()
-                color = re.compile(reg_val[10]).search(haye)
+                color = re.compile(color_rgx).search(haye)
                 if color:
-                    color_s = (memo, color.group())
+                    color_s = (memo, (color.group()).strip())
                     memo = ";".join(color_s)
                     haye = reader.readline()
                     logging.debug('color  %s; %s', memo, item)
                 else:
-                    logging.debug('color  %s; %s', memo, item)
+                    logging.debug('no color  %s; %s', memo, item)
                 prt_value(item, quantity, memo, price, record_dict, prt_list)
 
-            discount = re.compile(reg_val[11]).search(haye)
+            discount = re.compile(disc_rgx).search(haye)
             if discount:
                 disc_total += float(discount.group(2))
                 haye = reader.readline()
 
-            sale_tax = re.compile(reg_val[12]).search(haye)
+            sale_tax = re.compile(tax_rgx).search(haye)
             if sale_tax:
-                if not sale_tax.group(int(grp_val[3])):
+                if not sale_tax.group(int(tax_grp)):
                     tax_val = 0.00
                 else:
-                    tax_val = float(sale_tax.group(int(grp_val[3])))
+                    tax_val = float(sale_tax.group(int(tax_grp)))
                 prt_value(item_supply, 1, "SALES TAX", tax_val, record_dict, prt_list)
                 prt_value("Supplies", 1, "DISCOUNT", disc_total,record_dict, prt_list)
                 prt_value("Materials", 1, "PAINTCARE FEE", pca_total,record_dict, prt_list)
                 prt_value("Materials", 1, "SUPPLY CHAIN CHARGE", scc_total,record_dict, prt_list)
 
-
-                logging.debug('BREAK')
+                logging.debug('BREAK - sales tax')
                 break
 
+            logging.debug('this-line: %s  last-line:  %s', haye, haye_last)
             if haye == haye_last:
                 haye = reader.readline()
             haye_last = haye
 
-        rb = None
-        rec_b = re.compile(reg_val[1])
         while True:
             haye = reader.readline()
-            if not haye:
-                break
-            rb = rec_b.search(haye)
-            if rb:
+            ucode = re.search('\u000C', haye)
+            logging.debug("check unicode EOP %s", ucode)
+            if re.search('\u000C', haye):
                 logging.debug('parse to header')
                 break
-        logging.debug(str(haye))
+        logging.debug("Is new header? %s", str(haye))
+        haye = reader.readline()
         if not haye:
+            logging.debug("BREAK - EOF")
             break
