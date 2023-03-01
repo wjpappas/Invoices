336bc3bd3bbb7871cd7b2cd1766b170e46c76c2c
Bill Pappas <wjpappas@gmail.com>
2023-02-28 21:16:04 -0800

    refactor headers to include HD

---
+++ qb_head.py
@@ -0,0 +1,7 @@
+#   Output list templates
+qb_record = ['','','','','','','','','','','','','','','','','','','','','','','','','','','','Y',' ']
+qb_header = ['Vendor','Transaction Date','RefNumber','Bill Due','Terms','Memo','Address Line1','Address Line2',
+'Address Line3','Address Line4','Address City','Address State','Address PostalCode','Address Country','Vendor Acct No',
+'Expenses Account','Expenses Amount','Expenses Memo','Expenses Class','Expenses Customer','Expenses Billable','Items Item',
+'Items Qty','Items Description','Items Cost','Items Class','Items Customer','Items Billable','AP Account']
+header = 'Vendor,Transaction Date,RefNumber,Bill Due,Terms,Memo,Address Line1,Address Line2,Address Line3,Address Line4,Address City,Address State,Address PostalCode,Address Country,Vendor Acct No,Expenses Account,Expenses Amount,Expenses Memo,Expenses Class,Expenses Customer,Expenses Billable,Items Item,Items Qty,Items Description,Items Cost,Items Class,Items Customer,Items Billable,AP Account\n'
