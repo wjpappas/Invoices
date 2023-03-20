9129381d6d83d3d19a63600943a192e9d64f2a27
wjpappas <wjpappas@gmail.com>
2023-03-20 09:03:33 -0700

    clean out prints

--- block_head.py
+++ block_head.py
@@ -34,39 +34,26 @@ def make_dict(cust_file, oh_key):
         cfr = csv.reader(csvf)
         for line in cfr:
             (cust_job, jclass) = line
-
             cust_job = cust_job.strip('"')
-#            print(cust_job, jclass)
-
             jk = job_keyRegex.search(cust_job)
-
             if (jk):
                 job_key = jk.group(1)
                 cust_job_class = cust_job + jclass
-#                print(job_key,cust_job_class,'<<<<')
                 jobN_dict[job_key] = cust_job_class
-
     return jobN_dict
-
 
 def datex(idate, termd):
 
     datx = datetime.strptime(idate, "%m/%d/%Y")
-    m1 = datx.month
-    y1 = datx.year
-    d1 = datx.day
+    m1, y1, d1 = datx.month, datx.year, datx.day
 
-#   print(m1,d1,y1)
     if d1 >= 1:
         m1 = m1 + 1
     if m1 == 13:
         m1 = 1
         y1 = y1 + 1
-#   print(m1,d1,y1)
     a1 = datx.replace(month=m1, day=termd, year=y1)
-#   print(a1)
     date_due = a1.strftime("%m/%d/%Y")
-#   print(date_due)
     return date_due
 
 def read_vendor(file):
